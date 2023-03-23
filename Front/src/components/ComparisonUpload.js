324a2820f09bb6e651e3cd6ab4a95a91a85ba654
LeaJaeyoung <pt0108@naver.com>
2023-03-23 15:57:02 +0900

    add component css

--- Front/src/components/ComparisonUpload.js
+++ Front/src/components/ComparisonUpload.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Container } from "react-bootstrap";
+import "../styles/ComparisionUpload.css";
 
 function ComparisonUpload() {
     const [file, setFile] = useState({});
@@ -14,16 +15,12 @@ function ComparisonUpload() {
     }
 
     return (
-        <Container style={{ width: "100%", marginTop: "50px" }}>
-            <div style={{ width: "350px", margin: "0 auto" }}>
-                <h4 style={{ textAlign: "center", fontWeight: "bold", marginBottom: "10px" }}>영상을 업로드해주세요.</h4>
+        <Container id="file-upload-container">
+            <div>
+                <h4>영상을 업로드해주세요.</h4>
                 <input type="file" onChange={fileUpload} />
                 {file.video && <video src={file.url} controls width="100%" style={{ marginTop: "20px"}} />}
-                {file.video && 
-                    <button 
-                        style={{ border: "1px solid #40513B", color:"white", backgroundColor:"#609966", 
-                                 borderRadius: "5px", padding: "2px 8px" }}>
-                    분석 시작</button>}
+                {file.video && <button id="file-upload-btn">분석 시작</button>}
             </div>  
         </Container>
 
