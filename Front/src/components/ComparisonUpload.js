663019b922a16cde09a137d3112ecdcdb1c22b20
LeaJaeyoung <pt0108@naver.com>
2023-03-24 13:07:44 +0900

    분석 화면 추가

--- Front/src/components/ComparisonUpload.js
+++ Front/src/components/ComparisonUpload.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Container } from "react-bootstrap";
-import "../styles/ComparisionUpload.css";
+import "../styles/ComparisonUpload.css";
 
 function ComparisonUpload() {
     const [file, setFile] = useState({});
@@ -17,7 +17,7 @@ function ComparisonUpload() {
     return (
         <Container id="file-upload-container">
             <div>
-                <h4>영상을 업로드해주세요.</h4>
+                <h3>영상을 업로드해주세요.</h3>
                 <input type="file" onChange={fileUpload} />
                 {file.video && <video src={file.url} controls width="100%" style={{ marginTop: "20px"}} />}
                 {file.video && <button id="file-upload-btn">분석 시작</button>}
