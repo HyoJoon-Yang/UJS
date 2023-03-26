f5a9b524bb1b2bac59372cb640ff0481bf123f0a
LeaJaeyoung <pt0108@naver.com>
2023-03-26 10:34:47 +0900

    내 프로필 수정

--- Front/src/pages/Account.js
+++ Front/src/pages/Account.js
@@ -1,5 +1,7 @@
 import Layout from "../components/layout/Layout.js";
 import UserAccountModal from "../components/UserAccountModal.js";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import "../styles/Account.css";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -16,6 +18,12 @@ function Account() {
                         <h3><b>User name</b></h3>
                         <p>게시글 0  |  <Link to="/user-list">진단목록</Link> 0</p>
                     </span>
+                </Container>
+                <Container fluid id="account-article">
+                    <Row>
+                        <Col sm={5}>점수 랭킹 표시 구역</Col>
+                        <Col sm={7}>그래프 표시 구역</Col>
+                    </Row>
                 </Container>
             </Container>
         </Layout>
