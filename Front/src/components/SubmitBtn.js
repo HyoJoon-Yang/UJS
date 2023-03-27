a60f32e3d69072dcd6cd3033703a16a2889dad89
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-03-28 06:13:12 +0900

    Update SubmitBtn.js

--- Front/src/components/SubmitBtn.js
+++ Front/src/components/SubmitBtn.js
@@ -1,7 +1,48 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+
+import axios from "axios";
 // 로그아웃 버튼과 모달창 버튼의 css는 Navbar.css에 적어둠
+const handleSubmit = async (event) => {
+    var s_board  = document.getElementById("select_value");
+    var value = (s_board.options[s_board.selectedIndex].value);
+    var board_type='';
+    // eslint-disable-next-line default-case
+    switch (value) {
+        case "1":
+          board_type="posts";
+          break;
+        case "2":
+          board_type="notices";
+          break;
+        case "3":
+          // eslint-disable-next-line no-unused-vars
+          board_type="suggest";
+          break;
+    }
+    event.preventDefault();
+    
+    if (board_type !== "") {
+        const response = await axios.post("http://127.0.0.1:8000/api/v1/commu/posts/", {title: event.target.title.value,
+                                                                                        content: event.target.content.value,
+                                                                                        kind:board_type,
+
+        })
+                            .then(function (response) {
+                                console.log(response);
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            });              
+        console.log(response.data);
+       
+        }
+        else {
+            alert("게시판 종류를 선택하세요!")
+            
+        }
+    };
 
 function SubmitBtn() {
 
@@ -9,7 +50,7 @@ function SubmitBtn() {
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-
+    
     return(
         <div>
             <Button id="logout_btn" className="btn" onClick={handleShow}>작성</Button>
@@ -23,7 +64,7 @@ function SubmitBtn() {
                     <Button className="btn_close logout_modal_btn" onClick={handleClose}>
                         닫기
                     </Button>
-                    <Button style={{ backgroundColor: "#9DC08B", border: "none" }} type="submit" className="logout_modal_btn">확인</Button>
+                    <Button style={{ backgroundColor: "#9DC08B", border: "none" }} onClick={handleSubmit} className="logout_modal_btn">확인</Button>
                 </Modal.Footer>
             </Modal>
         </div>
