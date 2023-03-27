35f0f206fb534c62f40e99b29687520e7d82db90
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-03-28 05:29:32 +0900

    Update Notice.js

--- Front/src/pages/Notice.js
+++ Front/src/pages/Notice.js
@@ -3,15 +3,38 @@ import Button from 'react-bootstrap/Button';
 import Banner from "../components/Banner.js";
 import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import BoardList from "../components/BoardList.js";
+import Table from 'react-bootstrap/Table';
+import {useGetData} from "./UseGetData.js";
 
+import "../styles/BoardList.css";
 function Notice() {
+    const text = useGetData("http://127.0.0.1:8000/api/v1/commu/posts/")
     return (
         <Layout>
             <Container style={{minHeight: '100vh'}}>
                 <Banner title="공지사항" />
-                <BoardList />
-                <Link to="/post-form">
+                <Table id="board-list-table" striped bordered hover size="sm">
+                    <tbody>
+                            <>
+                        {text.filter((e) => {
+                                    return e.kind === "notices"; // notice
+                                    }).map((e) => (
+                            
+                                <tr>
+                                    
+                                            <td onClick="http://localhost:3000/commu" className="first-td">{e.id}</td>
+                                            <td><Link to={`/commu-detail/${e.id}`}>{e.title}</Link></td>  
+                                            <td className="last-td">{e.create_at}</td>
+                            
+
+                                        
+                                </tr>
+                        ))}
+                        </>
+               
+            </tbody>
+            </Table>
+            <Link to="/post-form">
                     <Button
                         style={{ backgroundColor: "#9DC08B", border: "none", borderRadius: "10px",
                                 float: "right", marginTop: "10px", marginRight: "10px" }}
