1efa7b3ee307ab10dd6c8335143e3bc0475e7095
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-03-28 06:13:10 +0900

    Update PostRemoveModal.js

--- Front/src/components/PostRemoveModal.js
+++ Front/src/components/PostRemoveModal.js
@@ -2,16 +2,23 @@ import React, { useState } from 'react';
 // import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { useParams } from 'react-router-dom'; 
+import { Link } from "react-router-dom";
+import axios from "axios";
 // 로그아웃 버튼과 모달창 버튼의 css는 Navbar.css에 적어둠
 
 function PostRemoveModal() {
+    const {id : postId} = useParams();
+    const [text, setText] = useState([]);
+    
 
     const [show, setShow] = useState(false);
-
+    
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-
     return(
+        
+    
         <div>
             <Button
                 className="commu-detail-btn" onClick={handleShow}>
@@ -23,11 +30,16 @@ function PostRemoveModal() {
                     <Modal.Title></Modal.Title>
                 </Modal.Header> */}
                 <Modal.Body>정말 삭제하시겠습니까?</Modal.Body>
+                
                 <Modal.Footer>
                     <Button className="btn_close logout_modal_btn" onClick={handleClose}>
                         닫기
                     </Button>
-                    <Button className="logout_modal_btn">게시글 삭제</Button>
+                    <Link to="/commu"><Button 
+              onClick={() => {
+                axios.delete(`http://127.0.0.1:8000/api/v1/commu/posts/${postId}`);
+                setText(text.filter((text) => text.id !== postId));
+              }} className="logout_modal_btn">게시글 삭제</Button></Link>
                 </Modal.Footer>
             </Modal>
         </div>
