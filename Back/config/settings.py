7f9b656191c369952b134b9f8a76ce9a5f691d76
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-03-28 05:28:56 +0900

    Update settings.py

--- Back/config/settings.py
+++ Back/config/settings.py
@@ -41,8 +41,8 @@ THIRD_PARTY_APPS = [
 CUSTOM_APPS = [
     "users.apps.UsersConfig",
     "commu.apps.CommuConfig",
-    "medias.apps.MediasConfig",
-    "analyses.apps.AnalysesConfig",
+#    "medias.apps.MediasConfig",
+#    "analyses.apps.AnalysesConfig",
 ]
 
 
