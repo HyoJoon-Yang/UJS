2bb97420d24d28029728b1be684cf1343f4fabf2
LeaJaeyoung <pt0108@naver.com>
2023-03-28 11:07:24 +0900

    타입스크립트 수정

--- Front(typescript_ver)/src/pages/UserList.tsx
+++ Front(typescript_ver)/src/pages/UserList.tsx
@@ -5,35 +5,40 @@ import "../styles/UserList.css";
 import React from "react";
 import { Layout } from "src/components/layout/Layout";
 import AccountHeader from "src/components/AccountHeader";
+import Form from 'react-bootstrap/Form';
+import "../styles/UserList.css";
+import UserListMoreBtn from "src/components/UserListMoreBtn";
 
 export default function UserList() {
   return (
     <Layout>
-      <Container style={{ minHeight: "100vh", position: "relative" }}>
-        <AccountHeader />
-        <h3>
-          <b>내 진단목록</b>
-        </h3>
-        <Container className="user-list-box">
-          <h5>2023-01-03 17:30 제목 없음</h5>
-          <h6>측면 스윙 | 8.5 / 10 점</h6>
-          <h6>+ 자세히 보기</h6>
-        </Container>
-        <Link
-          to="/account"
-          style={{
-            width: "100px",
-            position: "absolute",
-            bottom: "20px",
-            right: "10px",
-          }}
-        >
-          <ChevronLeftIcon boxSize={20} style={{ float: "left" }} />
-          <h6>
-            <b>뒤로가기</b>
-          </h6>
-        </Link>
-      </Container>
-    </Layout>
-  );
+            <Container style={{ minHeight: '100vh', position: "relative" }}>
+                <AccountHeader />
+                <Container id="user-list-heading">
+                    <h3><b>내 진단목록</b></h3>
+                    <Form.Select id="select-order" aria-label="Default select example">
+                        <option value="1">최신순</option>
+                        <option value="2">과거순</option>
+                    </Form.Select>
+                </Container>
+                <Container className="user-list-box">
+                    <h5>2023-01-25 18:23  ㅇㅇ골프장에서</h5>
+                    <h6>정면 스윙 | 8.2 / 10 점</h6>
+                    <UserListMoreBtn />
+                    <hr />
+                </Container>
+                <Container className="user-list-box">
+                    <h5>2023-01-03 17:30  제목 없음</h5>
+                    <h6>측면 스윙 | 8.5 / 10 점</h6>
+                    <UserListMoreBtn />
+                    <hr />
+                </Container>
+                <br />
+                <Link to="/account" id="user-list-back-btn">
+                    <ChevronLeftIcon boxSize={20} style={{ float:"left" }} />
+                    <h6><b>뒤로가기</b></h6>
+                </Link>
+            </Container>
+        </Layout>
+    );
 }
