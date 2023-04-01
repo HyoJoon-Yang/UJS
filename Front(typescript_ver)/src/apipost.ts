bcb978dfabebc036161c95512e843cf3e618da25
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-04-02 06:18:48 +0900

    Update apipost.ts

--- Front(typescript_ver)/src/apipost.ts
+++ Front(typescript_ver)/src/apipost.ts
@@ -1,12 +1,17 @@
 import axios from "axios";
 import { QueryFunctionContext } from "@tanstack/query-core";
-
+import Cookie from "js-cookie";
+import { Post,Comment } from "./types";
 const instance = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1/",
-});
+  withCredentials: true
+}); 
 
 export const getPosts = () => instance.get(`commu/posts/`).then((r) => r.data);
 
+
+export const getMe = () =>
+  instance.get(`users/me`).then((response) => response.data);
 
 export const getPostsDetail = async ({ queryKey }: QueryFunctionContext) => {
     const [_, postPk] = queryKey;
@@ -14,4 +19,55 @@ export const getPostsDetail = async ({ queryKey }: QueryFunctionContext) => {
       .get(`commu/posts/${postPk}`)
       .then((response) => response.data);
   };
-  export const getComments = () => instance.get(`comment/`).then((r) => r.data);
+
+  
+  export interface Owner{
+    avator: string,
+    name: string,
+    nickname: string,
+    email: string
+}
+  export interface Board {
+    title: string;
+    content: string;
+    kind: string;
+    owner: Owner;
+    image:string;
+  }
+  export const createBoard = ({ title, content, kind, owner,image }: Board) =>
+  instance
+    .post(
+      `commu/posts/`,
+      { title, content, kind,owner},
+      {
+        headers: {
+          "X-CSRFToken": Cookie.get("csrftoken") || "",
+        },
+      }
+    )
+    .then((response) => response.data);
+
+    export const deletePost = (postPk: number) =>
+    instance.delete(`commu/posts/${postPk}`, {
+      headers: {
+        "X-CSRFToken": Cookie.get("csrftoken") || "",
+      },
+    }).then((response) => response.data);
+
+
+
+
+    export const getComments = () => instance.get(`comment/`).then((r) => r.data);
+
+    export const createComment = ({ owner, post, contents }:Comment) =>
+    instance
+      .post(
+        `comment/`,
+        { owner, post, contents},
+        {
+          headers: {
+            "X-CSRFToken": Cookie.get("csrftoken") || "",
+          },
+        }
+      )
+      .then((response) => response.data);
