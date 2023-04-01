be4b8177fde32fa18f483b6000a5159f64aef6aa
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-04-02 06:18:33 +0900

    Update Faq.tsx

--- Front(typescript_ver)/src/pages/Faq.tsx
+++ Front(typescript_ver)/src/pages/Faq.tsx
@@ -1,19 +1,34 @@
 import Button from "react-bootstrap/Button";
+import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import BoardList from "../components/BoardList";
+import { Layout } from "../components/layout/Layout";
 import React from "react";
-import { Layout } from "src/components/layout/Layout";
-import Banner from "src/components/Banner";
-import BoardList from "src/components/BoardListSuggest";
-import ProtectedPage from "src/components/ProtectedPage";
+import ProtectedPage from "../components/ProtectedPage";
+import Table from "react-bootstrap/Table";
+import { useQuery } from "@tanstack/react-query";
+import { getPosts } from "../apipost";
+import { PostList } from "../types";
+
+
 
 export default function Faq() {
+  const { data} = useQuery<PostList[]>(["posts"], getPosts);
   return (
     <ProtectedPage>
       <Layout>
         <Container style={{ minHeight: "100vh" }}>
           <Banner title="건의사항" />
-          <BoardList />
+          <Table id="board-list-table" striped bordered hover size="sm">
+                      <tbody> 
+                      {data?.filter((e:any) => {
+                                return e.kind === "suggest";
+                                }).map((e:any) => (
+                        <BoardList kind={e.kind} pk={e.id} title={e.title} contents={""} user={e.user}  created_at={""} updated_at={""}/>
+                        ))}
+          </tbody>
+    </Table>
           <Link to="/post-form">
             <Button
               style={{
