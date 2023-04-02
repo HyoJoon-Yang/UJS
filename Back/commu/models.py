7ef1135acbae17be0e58a0c09d9d037962637f97
SeokJuGo <116260619+SeokJuGo@users.noreply.github.com>
2023-04-02 14:38:41 +0900

    Update models.py

--- Back/commu/models.py
+++ Back/commu/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Post(models.Model):
     class CategoryKindChoices(models.TextChoices):
@@ -9,31 +9,24 @@ class Post(models.Model):
     kind = models.CharField(max_length=10, blank=True, choices=CategoryKindChoices.choices)
     title = models.CharField(max_length=30)
     content = models.TextField()
-    create_at = models.DateField(auto_now_add=True)
-    update_at = models.DateField(auto_now=True)
-
-    owner = models.ForeignKey(
-        "users.User",
-        on_delete=models.CASCADE,
-        related_name="posts",
+    image = models.ImageField(
+        blank=True,
+        upload_to="post/%Y/%m/%d",
+        help_text="여기는 게시판들 이미지",
     )
-
-    def __str__(self):
-        return f"{self.title}"
-
-
-class Notice(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    
     create_at = models.DateField(auto_now_add=True)
     update_at = models.DateField(auto_now=True)
     
     owner = models.ForeignKey(
         "users.User",
         on_delete=models.CASCADE,
-        related_name="notices",
+        related_name="posts",
     )
-
+    
+    def get_absolute_url(self):
+        return reverse("front:commu_detail", args=[self.pk])
+        
     def __str__(self):
         return f"{self.title}"
     
