f1cab5a4e491d9e10fd69940c08b635a8818aebe
Dawnnote <lshoon7454@gmail.com>
2023-04-04 12:55:00 +0900

    Update AccountHeader

--- Front(typescript_ver)/src/components/AccountHeader.tsx
+++ Front(typescript_ver)/src/components/AccountHeader.tsx
@@ -2,16 +2,21 @@ import UserAccountModal from "../components/UserAccountModal";
 import "../styles/Account.css";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
+import useUser from "src/lib/useUser";
+import { Avatar, Text, WrapItem } from "@chakra-ui/react";
 
 export default function AccountHeader() {
+  const { user } = useUser();
   return (
     <Container fluid id="account-bg">
-      <div id="profile-img"></div>
+      {user?.avator ? <Avatar id="profile-img" src={`http://localhost:8000${user?.avator}`} /> : <Avatar id="profile-img"/>}
+      
+
       <span id="profile-desc">
         <UserAccountModal />
         <h3>
-          <b>User name</b>
+          <Text as="b">{user?.nickname}</Text>
         </h3>
         <p>
           게시글 0 | <Link to="/user-list">진단목록</Link> 0
@@ -20,3 +25,20 @@ export default function AccountHeader() {
     </Container>
   );
 }
+
+// export default function AccountHeader() {
+//   return (
+//     <Container fluid id="account-bg">
+//       <div id="profile-img"></div>
+//       <span id="profile-desc">
+//         <UserAccountModal />
+//         <h3>
+//           <b>User name</b>
+//         </h3>
+//         <p>
+//           게시글 0 | <Link to="/user-list">진단목록</Link> 0
+//         </p>
+//       </span>
+//     </Container>
+//   );
+// }
