7768dbbb40d667bbd114f8dc0646b39cbb20a756
LeaJaeyoung <106129152+pt0108@users.noreply.github.com>
2023-04-07 12:57:50 +0900

    Update ComparisonResult.tsx

--- Front(typescript_ver)/src/components/ComparisonResult.tsx
+++ Front(typescript_ver)/src/components/ComparisonResult.tsx
@@ -1,7 +1,6 @@
 import { Container } from "react-bootstrap";
-import { useState } from "react";
+import React, { useEffect, useState } from 'react';
 import "../styles/ComparisonResult.css";
-import React from "react";
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Popover from 'react-bootstrap/Popover';
 import Tab from 'react-bootstrap/Tab';
@@ -10,7 +9,9 @@ import { EmailShareButton, EmailIcon, FacebookShareButton, FacebookIcon, LineSha
 
 
 export default function ComparisonResult() {
+
   const currentUrl = window.location.href;
+
   const popover = (
       <Popover id="popover-basic">
         <Popover.Body>
@@ -31,7 +32,14 @@ export default function ComparisonResult() {
       </Popover>
     );
 
-  // const [ isOn, setIsOn ] = useState(true);
+    const [comparisons, setComparisons] = useState([]);
+
+    useEffect(() => {
+      fetch('/api/photos/')
+        .then(response => response.json())
+        .then(data => setComparisons(data));
+    }, []);
+
   return (
     <Container fluid id="comparison-result-container">
       <h3>분석 결과</h3>
@@ -44,73 +52,19 @@ export default function ComparisonResult() {
         id="uncontrolled-tab-example"
         className="mb-3"
         >
-          <Tab eventKey="1" title="adress">
-            <img src="img/adress.png" alt="adress picture" style={{ width: "100%" }} />
-            <h6>
-              헤드 위치: PASS ✅ <br/>
-              팔 간격: PASS ✅ <br/>
-              골반 위치: PASS ✅ <br/>
-              스탠스: PASS ✅ <br/>
-              <hr/>
-              <b>100점</b>
-            </h6>
-          </Tab>
-          <Tab eventKey="2" title="backswing">
-            <img src="img/backswing.png" alt="backswing picture" style={{ width: "100%" }} />
-            <h6>
-              헤드 위치: 왼쪽으로 중심이 기울어졌습니다. ❌ <br/>
-              팔 간격: 팔을 좀 더 펴주세요. ❌ <br/>
-              골반 각도: PASS ✅ <br/>
-              <hr/>
-              <b>33점</b>
-            </h6>
-          </Tab>
-          <Tab eventKey="3" title="backswingtop">
-            <img src="img/backswingtop.png"alt="backswingtop picture" style={{ width: "100%" }} />
-            <h6>
-              헤드 위치: 왼쪽으로 중심이 기울어졌습니다. ❌ <br/>
-              팔 간격: 왼쪽으로 쏠렸습니다. ❌ <br/>
-              골반 각도: PASS ✅ <br/>
-              <hr/>
-              <b>33점</b>
-            </h6>
-          </Tab>
-          <Tab eventKey="4" title="impact">
-            <img src="img/impact.png" alt="impact picture" style={{ width: "100%" }} />
-            <h6>
-              헤드 위치: PASS ✅ <br/>
-              골반 위치: PASS ✅ <br/>
-              왼쪽 어깨 위치: PASS ✅ <br/>
-              손 위치: PASS ✅ <br/>
-              <hr/>
-              <b>100점</b>
-            </h6>
-          </Tab>
-          <Tab eventKey="5" title="follow">
-            <img src="img/follow.png" alt="follow picture" style={{ width: "100%" }} />
-            <h6>
-              헤드 위치: PASS ✅ <br/>
-              골반 다리: PASS ✅ <br/>
-              오른쪽 다리: PASS ✅ <br/>
-              <hr/>
-              <b>100점</b>
-            </h6>
-          </Tab>
-          <Tab eventKey="6" title="finish">
-            <img src="img/finish.png" alt="finish picture" style={{ width: "100%" }} />
-            <h6>
-              헤드 위치: PASS ✅ <br/>
-              어깨 위치: 중심이 덜 이동하였습니다. ❌ <br/>
-              골반 위치: PASS ✅ <br/>
-              <hr/>
-              <b>100점</b>
-            </h6>
-          </Tab>
+          {comparisons.map(comparison => (
+            <Tab eventKey={comparison.id} title={comparison.title}>
+              <img key={comparison.id} src={comparison.url} alt="comparison photo" />
+              <h6>{comparison.text}</h6>
+              <hr />
+              <h6><b>{comparison.score}</b></h6>
+            </Tab>
+          ))}
         </Tabs>
       </Container>
 
       <Container id="comparison-result-text">
-        <h5>총점 : 77.7 점</h5>
+        <h5>총점 : {comparison.totalScore} 점</h5>
         <button className="result-btn">
           <img src="img/download.svg" alt="download btn" /> 결과 저장하기
         </button>
